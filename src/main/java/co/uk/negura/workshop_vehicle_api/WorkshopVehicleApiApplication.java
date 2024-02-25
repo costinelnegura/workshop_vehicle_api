61b4eaa3001dc41fc9b8b12b24fdd00cfdd3f3c3
costinelnegura <pigeons-conduit0n@icloud.com>
2024-02-25 14:54:52 +0000

    initial commit

--- src/main/java/co/uk/negura/workshop_vehicle_api/WorkshopVehicleApiApplication.java
+++ src/main/java/co/uk/negura/workshop_vehicle_api/WorkshopVehicleApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkshopVehicleApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkshopVehicleApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopVehicleApiApplication.class, args);
+    }
 
 }
