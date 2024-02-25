a5af9e7f482a750ce97896d7a40022c7d596c820
costinelnegura <pigeons-conduit0n@icloud.com>
2024-02-25 14:54:06 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# workshop_vehicle_api
---
+++ src/main/java/co/uk/negura/workshop_vehicle_api/WorkshopVehicleApiApplication.java
@@ -0,0 +1,13 @@
+package co.uk.negura.workshop_vehicle_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopVehicleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopVehicleApiApplication.class, args);
+	}
+
+}
